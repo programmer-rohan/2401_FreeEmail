f512a2dc8f5af42d67854363552b0c2f0d76d551
programmer-rohan <153588017+programmer-rohan@users.noreply.github.com>
2024-01-28 19:12:20 +0530

    Add files via upload

---
+++ API/Controllers/EmailController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace API.Controllers
+{
+    public class EmailModel
+    {
+        public string FromEmail { get; set; } = string.Empty;
+        public string ToEmails { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost("SendEmail")]
+        public ActionResult SendEmail(EmailModel emailData)
+        {
+            var message = new MailMessage()
+            {
+                From = new MailAddress(emailData.FromEmail),
+                Subject = emailData.Subject,
+                IsBodyHtml = true,
+                Body = $"""
+                <html>
+                    <body>
+                        <h3>{emailData.Body}</h3>
+                    </body>
+                </html>
+                """
+            };
+            foreach(var toEmail in emailData.ToEmails.Split(";"))
+            {
+                message.To.Add(new MailAddress(toEmail));
+            }
+
+            var smtp = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(emailData.FromEmail, "your key"),
+                EnableSsl = true,
+            };
+
+            smtp.Send(message);
+
+            return Ok("Email Sent!");
+        }
+    }
+}
